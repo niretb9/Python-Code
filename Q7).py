7dc42522aba917e4800f9e12470cb587a1bbca84
cosmicplayer69 <niretb9@gmail.com>
2022-01-18 23:34:45 +0530

    second-commit

--- Q7).py
+++
@@ -1,8 +0,0 @@
-# Python program to find sum of elements in list
-total = 0
-numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
- 
-for ele in range(0, len(numbers)):
-    total = total + numbers[ele]
- 
-print("Sum of all elements in given list: ", total)
