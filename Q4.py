1775782a766ca3f61aa5733a9e51a0a06801eae1
Niret Badgire <69514954+niretb9@users.noreply.github.com>
2024-08-19 01:27:39 +0530

    Delete Q4.py

--- Q4.py
+++
@@ -1,7 +0,0 @@
-#What does a dictionary value with a key 'foo' and a value 42 look like ?
-#answer:- {'foo': 42}
-
-people = {1: {'name': 'Michael', 'age': '42', 'sex': 'Male', 'height': '160cm', 'weight': '65kg'},
-          2: {'name': 'Holly', 'age': '39', 'sex': 'Female', 'height': '156cm', 'weight': '50kg'}}
-
-print(people)
