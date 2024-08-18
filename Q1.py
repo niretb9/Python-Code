3c6c3c57fa545e1848fb6fb23d781ec2053c0fd0
Niret Badgire <69514954+niretb9@users.noreply.github.com>
2024-08-19 01:26:12 +0530

    Delete Q1.py

--- Q1.py
+++
@@ -1,9 +0,0 @@
-#program for positive, negative ,or zero
-num = float(input("Enter a number: "))
-if num >= 0:
-   if num == 0:
-       print("Zero")
-   else:
-       print("Positive number")
-else:
-   print("Negative number")
